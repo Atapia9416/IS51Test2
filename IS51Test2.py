8f81bd769bc942e2e65755e9937a73563938a623
Alan <atapia9416@mail.fresnostate.edu>
2021-04-29 17:29:47 -0700

    Pushing structured english

---
+++ IS51Test2.py
@@ -0,0 +1,20 @@
+"""
+=========
+Question 1: Structured English
+=========
+
+The program will take in a list of grades from a final exam and process 
+the data to output the amount of grades, average grades, and the percentage 
+of grades that are above the average grade.
+
+The first part will read the text file using infile and make the data into a list of strings to  be able to format
+them from python. You then take the list and make it into integers.
+
+Then for the first calculation, the lens command will read the amount of
+strings in the list and set it equal to total grades variable.
+
+The next command will find the average by calculting the sum of the list and dividing it by the len.
+
+and finally you will need to calculate the amount of integers that are above the average by using a for/in command.
+
+"""
